2e12c9ecef81a23201fb6f9d98e96a1935400421
Miguel Ramirez <themike123me@gmail.com>
2019-08-28 17:53:13 -0500

    Se creo un layout base.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,12 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Home extends CI_Controller {
 	//Declare path layout base
 	private $layout_base = 'layout/base';
 
 	public function index()
 	{
-		$this->load->view($this->layout_base);
+		$data['view']='home/index';
+		$data['tittle']='Home';
+
+		$this->load->view($this->layout_base,$data);
 	}
 }
---
+++ application/views/home/index.php
@@ -0,0 +1,6 @@
+<div class="row" style="margin-top: 4.5rem">
+    <div class="col">
+        <h1>Start Programing</h1>
+    </div>
+</div>
+
